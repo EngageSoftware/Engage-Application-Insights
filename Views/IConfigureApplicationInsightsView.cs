a04aee7b1ca3391bd63bc7530c97c3b4425db56c
Brian Dukes <bdukes@engagesoftware.com>
2016-11-01 13:58:11 -0500

    Configure copyright for Style Cop

--- Views/IConfigureApplicationInsightsView.cs
+++ Views/IConfigureApplicationInsightsView.cs
@@ -1,6 +1,6 @@
 ﻿// <copyright file="IConfigureApplicationInsightsView.cs" company="Engage Software">
 // Engage: Application Insights
-// Copyright (c) 2004-2015
+// Copyright (c) 2004-2016
 // by Engage Software ( http://www.engagesoftware.com )
 // </copyright>
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
